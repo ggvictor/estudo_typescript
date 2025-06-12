de18bb61283b3652dc44a8dbbd4ddbfcf7ae0b9b
ggvictor <victorrodriguessilva2@gmail.com>
2025-06-12 18:21:57 -0300

    voltando

--- 4_react_com_ts/src/App.tsx
+++ 4_react_com_ts/src/App.tsx
@@ -1,13 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+function App(){
+    // 1 - variaveis
+    const name: string = "Victor"
+    const age: number = 21;
+    const isWorking:boolean = true;
 
-function App() {
-  return (
-    <div className="App">
-      <h1>TypeScript com React</h1>
-    </div>
-  );
+
+    // 2 - funções 
+    const userGreeting = (name:string): string =>{
+        return `Olá, ${name}`
+    }
+    return(
+        <div className="App">
+            <h1>Typescript com React</h1>
+            <h2>Nome: {name}</h2>
+            <p>Idade: {age}</p>
+            {isWorking &&(
+                <p>Esta trabalhando</p>
+            )}
+            <h3>{userGreeting(name)}</h3>
+        </div>
+    );
 }
 
-export default App;
+export default App
